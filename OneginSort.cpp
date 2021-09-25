6932552719518895ddf83481383c7ac43acc6634
Roman <korostinckii.r@phystech.edu>
2021-09-26 01:03:34 +0300

    Made all sorts

--- OneginSort.cpp
+++ OneginSort.cpp
@@ -21,15 +21,25 @@ struct Text
 
 void ReadFile (struct Text *text);
 
+void WriteFile (struct Text *text);
+
 void MakeString (struct Text *text);
 
 size_t DeleteSpaces (char *array);
 
-int IsSpaceOrTab (char ch);
-
-int AlphabetOrderCMP(const void *arg1, const void *arg2);
+int AlphabetOrderCMP (const void *arg1, const void *arg2);
 
 void AlphabetOrderSort(struct Text *text);
+
+int RhymeCMP (const void *arg1, const void *arg2);
+
+void RhymeSort (struct Text *text);
+
+int OriginalCMP (const void *arg1, const void *arg2);
+
+void OriginalSort (struct Text *text);
+
+void TestPrint (struct Text *text);
 
 int main()
 {
@@ -41,9 +51,15 @@ int main()
 
 	AlphabetOrderSort(&hamlet);
 
-	for (size_t i = 0; i < hamlet.num_of_strings; i++)
-		printf("%d\t%s\n", hamlet.strings[i].len, hamlet.strings[i].ptr);
-	printf("\n\n");
+	RhymeSort(&hamlet);
+
+	WriteFile(&hamlet);
+
+	OriginalSort(&hamlet);
+
+	//TestPrint(&hamlet);
+
+	//WriteFile(&hamlet);
 
 	free(hamlet.buffer);
 	free(hamlet.strings);
@@ -59,7 +75,7 @@ void ReadFile (struct Text *text)
 
 	text->buffer = (char*) calloc(text->size_of_file, sizeof(char));
 
-	FILE *fp;
+	FILE *fp = NULL;
 
 	fp = fopen(text->file_name, "r");
 
@@ -69,17 +85,32 @@ void ReadFile (struct Text *text)
 	fclose(fp);
 }
 
+void WriteFile (struct Text *text)
+{
+	text->file_name = "hamlet.txt";
+
+	FILE *fp = NULL;
+
+	fp = fopen(text->file_name, "w");
+
+	for (size_t i = 0; i < text->num_of_strings; i++)
+		fprintf(fp, "%s\n", text->strings[i].ptr);
+	printf("\n");
+}
+
 void MakeString (struct Text *text)
 {
 	text->num_of_strings = DeleteSpaces(text->buffer);
 
-	text->strings = (struct String*) calloc(text->num_of_strings, sizeof(struct String));
+	text->strings = (struct String*) calloc(text->num_of_strings, 
+		sizeof(struct String));
 
 	char *string_ptr = text->buffer;
 	char *newline_pos = NULL;
 	int string_ctr = 0;
 
-	while ((newline_pos = strchr (string_ptr, '\n')) || (newline_pos = strchr (string_ptr, EOF)))
+	while ((newline_pos = strchr (string_ptr, '\n')) || 
+		(newline_pos = strchr (string_ptr, EOF)))
 	{
 		if((long unsigned int)string_ctr >= text->num_of_strings) 
 		{
@@ -100,13 +131,14 @@ void MakeString (struct Text *text)
 size_t DeleteSpaces (char *array)
 {	
 	int i, j;
-	int is_newline = false;
+	int is_newline = true;
+	int is_space = true;
 	int num_of_strings = 0;
 	for (i = 0, j = 0; array[i] != EOF; i++)
 	{
 		if (array[i] == '\n')
 		{
-			if(array[i + 1] != '\n' && array[i + 1] != 0)
+			if(!is_newline && array[i + 1] != '\0')
 			{
 				array[j++] = array[i];
 				is_newline = true;
@@ -116,26 +148,25 @@ size_t DeleteSpaces (char *array)
 				continue;
 		}
 
-		else if (IsSpaceOrTab(array[i]))
+		else if (isspace(array[i]))
 		{
-			if (!isspace(array[i + 1]) && !is_newline)
+			if (!is_space && !is_newline)
+			{
 				array[j++] = array[i];
+				is_space = true;
+			}
 			else
 				continue;
 		}
-		else if(array[i] != 0)
+		else if (array[i] != '\0')
 		{
 			array[j++] = array[i];
 			is_newline = false;
+			is_space = false;
 		}
 	}
 	array[j] = '\n';
 	return num_of_strings + 1;
-}
-
-int IsSpaceOrTab (char ch)
-{
-	return (ch == ' ' || ch == '\t') ? 1 : 0;
 }
 
 int AlphabetOrderCMP(const void *arg1, const void *arg2)
@@ -147,15 +178,90 @@ int AlphabetOrderCMP(const void *arg1, const void *arg2)
 	char *ptr2 = str2->ptr;
 
 	while (!isalpha(*ptr1))
+	{
+		if(ptr1 <= str1->ptr + str1->len - 1)
+			break;
 		ptr1++;
+	}
 
 	while (!isalpha(*ptr2))
+	{
+		if(ptr2 <= str2->ptr + str2->len - 1)
+			break;
 		ptr2++;
+	}
 
 	return strcmp(ptr1, ptr2);
 }
 
 void AlphabetOrderSort(struct Text *text)
 {
-	qsort(text->strings, text->num_of_strings, sizeof (struct String), AlphabetOrderCMP);
+	qsort(text->strings, text->num_of_strings, sizeof (struct String), 
+		AlphabetOrderCMP);
+}
+
+int RhymeCMP (const void *arg1, const void *arg2)
+{
+	struct String *str1 = (struct String *) arg1;
+	struct String *str2 = (struct String *) arg2;
+
+	char *ptr1 = str1->ptr + str1->len - 2;
+	char *ptr2 = str2->ptr + str2->len - 2;
+
+	// if (!isalpha(*ptr1))
+	// 	ptr1--;
+
+	// if (!isalpha(*ptr2))
+	// 	ptr2--;
+
+	while (!isalpha(*ptr1))
+	{
+		if (ptr1 <= str1->ptr)
+			break;
+		ptr1--;
+	}
+
+	while (!isalpha(*ptr2))
+	{
+		if (ptr2 <= str2->ptr)
+			break;
+		ptr2--;
+	}
+
+	while (*ptr1 == *ptr2)
+	{
+		if (ptr1 == str1->ptr && ptr2 == str2->ptr)
+			return 0;
+		ptr1--;
+		ptr2--;
+	}
+
+	return *ptr1 - *ptr2;
+}
+
+void RhymeSort (struct Text *text)
+{
+	qsort(text->strings, text->num_of_strings, sizeof (struct String), 
+		RhymeCMP);
+}
+
+int OriginalCMP (const void *arg1, const void *arg2)
+{
+	struct String *str1 = (struct String *) arg1;
+	struct String *str2 = (struct String *) arg2;
+
+	return str1->ptr - str2->ptr;
+}
+
+void OriginalSort (struct Text *text)
+{
+	qsort(text->strings, text->num_of_strings, sizeof (struct String), 
+		OriginalCMP);
+}
+
+void TestPrint (struct Text *text)
+{
+	for (size_t i = 0; i < text->num_of_strings; i++)
+		printf("%d\t%s\n", text->strings[i].len, text->strings[i].ptr);
+	printf("\n");
 }
