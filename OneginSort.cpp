1a7764989fe20b12961f3bccc9d581fe4385d775
Roman <korostinckii.r@phystech.edu>
2021-09-25 18:13:23 +0300

    Fixed some bugs and add an alphabet order sort

--- OneginSort.cpp
+++ OneginSort.cpp
@@ -1,14 +1,8 @@
-/* Проблемы:
-- считывается лишний \n
-- в MakeString не дедтектируется eof в цикле
-- MakeString не работает впринципе*/
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-
-const int SIZE_OF_FILE = 200000;
+#include <cassert>
 
 struct String
 {
@@ -22,7 +16,7 @@ struct Text
 	size_t size_of_file;
 	char *buffer;
 	size_t num_of_strings;
-	struct String *file_strings;
+	struct String *strings;
 };
 
 void ReadFile (struct Text *text);
@@ -33,72 +27,78 @@ size_t DeleteSpaces (char *array);
 
 int IsSpaceOrTab (char ch);
 
+int AlphabetOrderCMP(const void *arg1, const void *arg2);
+
+void AlphabetOrderSort(struct Text *text);
 
 int main()
 {
 	struct Text hamlet;
 
-	ReadFile(&hamlet); //добавиь возможность вводить название файла с клавиатуры
+	ReadFile(&hamlet);
 
 	MakeString(&hamlet);
 
+	AlphabetOrderSort(&hamlet);
+
+	for (size_t i = 0; i < hamlet.num_of_strings; i++)
+		printf("%d\t%s\n", hamlet.strings[i].len, hamlet.strings[i].ptr);
+	printf("\n\n");
+
 	free(hamlet.buffer);
-	free(hamlet.file_strings);
+	free(hamlet.strings);
+
+	return 0;
 }
 
-void ReadFile (struct Text *text) //добавить информацию об открытии файла
+void ReadFile (struct Text *text)
 {
 	text->file_name = "hamlet.txt";
 
 	text->size_of_file = 200000;
 
-	text->buffer = (char*) calloc(text->size_of_file + 1, sizeof(char));
+	text->buffer = (char*) calloc(text->size_of_file, sizeof(char));
 
 	FILE *fp;
 
 	fp = fopen(text->file_name, "r");
 
-	fread(text->buffer, sizeof(char), text->size_of_file, fp);
-	text->buffer[text->size_of_file] = EOF;
+	int sz = fread(text->buffer, sizeof(char), text->size_of_file, fp);
+	text->buffer[sz] = EOF;
 
-	fclose(fp);	
+	fclose(fp);
 }
 
 void MakeString (struct Text *text)
 {
 	text->num_of_strings = DeleteSpaces(text->buffer);
 
-	text->file_strings = (struct String*) calloc(text->num_of_strings, sizeof(struct String));
+	text->strings = (struct String*) calloc(text->num_of_strings, sizeof(struct String));
 
 	char *string_ptr = text->buffer;
 	char *newline_pos = NULL;
 	int string_ctr = 0;
 
-	while ((newline_pos = strchr (string_ptr, '\n')))
+	while ((newline_pos = strchr (string_ptr, '\n')) || (newline_pos = strchr (string_ptr, EOF)))
 	{
-		*newline_pos = '\0'; //
+		if((long unsigned int)string_ctr >= text->num_of_strings) 
+		{
+			*newline_pos = '\0';
+			break;
+		}
+		*newline_pos = '\0';
 
-		text->file_strings[string_ctr].ptr = string_ptr;
-		text->file_strings[string_ctr].len = newline_pos - string_ptr + 1;
+		text->strings[string_ctr].ptr = string_ptr;
+		text->strings[string_ctr].len = newline_pos - string_ptr + 1;
 
-		string_ptr = newline_pos + 1;	
+		string_ptr = newline_pos + 1;
 
 		string_ctr++;
 	}
-
-/////////////////////////
-	for (size_t i = 0; i < text->num_of_strings; i++)
-		printf("%s\t%d\n", text->file_strings[i].ptr, text->file_strings[i].len);
-/////////////////////////
-	//for (int i = 0; text->buffer[i] != EOF; i++)
-		//printf("%c", text->buffer[i]);
-	printf("\nnum_of_strings =  %ld , %d\n", text->num_of_strings, string_ctr + 1);
-/////////////////////////
-
 }
 
 size_t DeleteSpaces (char *array)
-{
+{	
 	int i, j;
 	int is_newline = false;
 	int num_of_strings = 0;
@@ -106,7 +106,7 @@ size_t DeleteSpaces (char *array)
 	{
 		if (array[i] == '\n')
 		{
-			if(array[i + 1] != '\n' && array[i + 1] != EOF) //считывает последний /n перед EOF
+			if(array[i + 1] != '\n' && array[i + 1] != 0)
 			{
 				array[j++] = array[i];
 				is_newline = true;
@@ -123,22 +123,39 @@ size_t DeleteSpaces (char *array)
 			else
 				continue;
 		}
-		else
+		else if(array[i] != 0)
 		{
 			array[j++] = array[i];
 			is_newline = false;
 		}
 	}
-
-	array[j] = EOF;
-
+	array[j] = '\n';
 	return num_of_strings + 1;
 }
 
 int IsSpaceOrTab (char ch)
 {
-	if (ch == ' ' || ch == '\t')
-		return 1;
-	else
-		return 0;
+	return (ch == ' ' || ch == '\t') ? 1 : 0;
+}
+
+int AlphabetOrderCMP(const void *arg1, const void *arg2)
+{
+	struct String *str1 = (struct String *) arg1;
+	struct String *str2 = (struct String *) arg2;
+
+	char *ptr1 = str1->ptr;
+	char *ptr2 = str2->ptr;
+
+	while (!isalpha(*ptr1))
+		ptr1++;
+
+	while (!isalpha(*ptr2))
+		ptr2++;
+
+	return strcmp(ptr1, ptr2);
+}
+
+void AlphabetOrderSort(struct Text *text)
+{
+	qsort(text->strings, text->num_of_strings, sizeof (struct String), AlphabetOrderCMP);
 }
